0ac81fb73e440cca4b64bd80fc86b18fec6d7b0e
Soumyajit Das <soumyajitdas5402@gmail.com>
2023-01-27 17:33:05 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (64.86%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,6 +1,7 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
         int left = 0, right = nums.length - 1;
+        
         if (target < nums[left]) return 0;
         if (target > nums[right]) return nums.length;
         
@@ -10,7 +11,6 @@ class Solution {
             else if (nums[mid] < target) left = mid + 1;
             else right = mid;
         }
-        if (nums[left] == target) return left;
-        return nums[left] > target ? left : right;
+        return nums[left] >= target ? left : right;
     }
 }
