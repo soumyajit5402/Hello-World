98558f681c4f8d2ec868b95664477622b176cc18
Soumyajit Das <soumyajitdas5402@gmail.com>
2023-01-27 13:13:03 +0530

    Time: 9 ms (96.72%), Space: 41.7 MB (76.39%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) return false;
+        int rev = 0, temp = x;
+        while (x > 0) {
+            rev = rev * 10 + x % 10;
+            x = x / 10;
+        }
+        return temp == rev;
+    }
+}
