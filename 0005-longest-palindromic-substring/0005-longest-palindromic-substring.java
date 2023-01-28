32ffea013da774ca3549981528f81fbd50bd47fc
Soumyajit Das <soumyajitdas5402@gmail.com>
2023-01-28 22:07:42 +0530

    Time: 23 ms (77.40%), Space: 42.9 MB (46.83%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -2,8 +2,10 @@ class Solution {
     public String longestPalindrome(String s) {
         String ans = "";
         for (int index = 0; index < s.length(); index++) {
+            // check odd length palindromes
             String temp1 = checkPalindrome(s, index, index);
             if (temp1.length() > ans.length()) ans = temp1;
+            // check even length palindromes
             String temp2 = checkPalindrome(s, index - 1, index);
             if (temp2.length() > ans.length()) ans = temp2;
         }
