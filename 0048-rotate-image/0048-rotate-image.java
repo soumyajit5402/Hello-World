0d4a6c56882b18b5e5ae3f9b0b1f417b2d69a4a4
Soumyajit Das <soumyajitdas5402@gmail.com>
2023-01-31 04:41:35 +0530

    Time: 0 ms (100.00%), Space: 41 MB (71.52%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public void rotate(int[][] matrix) {
+        /*
+        The idea was firstly transpose the matrix and then flip it symmetrically.
+        After transpose, it will be -> swap(matrix[i][j], matrix[j][i])
+        Then flip the matrix horizontally -> (swap(matrix[i][j], matrix[i][matrix.length-1-j])
+        */
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i; j < matrix[0].length; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length - 1 - j];
+                matrix[i][matrix.length - 1 - j] = temp;
+            }
+        }
+    }
+}
