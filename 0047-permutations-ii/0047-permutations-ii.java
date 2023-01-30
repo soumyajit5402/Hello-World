998c34241812e2082e3352e2b88417a9945f13db
Soumyajit Das <soumyajitdas5402@gmail.com>
2023-01-31 04:29:07 +0530

    Time: 2 ms (80.94%), Space: 42.6 MB (99.29%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -1,28 +1,23 @@
 class Solution {
-    Set<Long> visited = new HashSet<>();
     List<List<Integer>> container = new ArrayList<>();
     public List<List<Integer>> permuteUnique(int[] nums) {
-        findPermutations(nums, 0l, 0);
+        findPermutations(nums, 0);
         return container;
     }
     
-    private void findPermutations(int[] arr, long uniqueKey, int index) {
+    private void findPermutations(int[] arr, int index) {
         if (index == arr.length) {
-            if (!visited.contains(uniqueKey)) {
-                List<Integer> currList = new ArrayList<>();
-                for (int num : arr) currList.add(num);
-                container.add(currList);
-            }
-            visited.add(uniqueKey);
+            List<Integer> currList = new ArrayList<>();
+            for (int num : arr) currList.add(num);
+            container.add(currList);
             return;
         }
         
+        Set<Integer> visited = new HashSet<>();
         for (int swapIndex = index; swapIndex < arr.length; swapIndex++) {
             swap(arr, index, swapIndex);
-            // compute unique key to avoid duplicates
-            long newUniqueKey = uniqueKey * 100l + (arr[index] + 10l); 
-            findPermutations(arr, newUniqueKey, index + 1);
-            swap(arr, index, swapIndex); // backtracking
+            if (visited.add(arr[index])) findPermutations(arr, index + 1);
+            swap(arr, index, swapIndex); // backtracking    
         }
     }
     
