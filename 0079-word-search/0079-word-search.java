76e8467f75c25f94c6c88a55cecad99aa24dd27d
Soumyajit Das <soumyajitdas5402@gmail.com>
2023-02-01 14:27:29 +0530

    Time: 262 ms (34.78%), Space: 42.6 MB (18.86%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -3,17 +3,17 @@ class Solution {
         boolean[][] visited = new boolean[board.length][board[0].length];
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
-                if (wordExists(board, i, j, visited, word, 0)) return true;
+                if (wordExists(board, i, j, visited, word.toCharArray(), 0)) return true;
             }
         }
         return false;
     }
     
-    private boolean wordExists(char[][] board, int x, int y, boolean[][] visited, String word, int index) {
-        if (index >= word.length()) return true;
+    private boolean wordExists(char[][] board, int x, int y, boolean[][] visited, char[] word, int index) {
+        if (index >= word.length) return true;
         if (x < 0 || x >= board.length || y < 0 || y >= board[0].length) return false;
         if (visited[x][y] == true) return false;
-        if (board[x][y] != word.charAt(index)) return false;
+        if (board[x][y] != word[index]) return false;
         
         visited[x][y] = true;
         int[] x_diff = new int[] { 1, -1, 0, 0 };
