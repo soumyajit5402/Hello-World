091307d4184337e803834f5e2397b00d8e5009f5
Soumyajit Das <soumyajitdas5402@gmail.com>
2023-02-01 13:06:06 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (72.15%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,7 +1,6 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int len = matrix[0].length, bre = matrix.length;
-        
         if (target < matrix[0][0] || target > matrix[bre-1][len-1]) return false;
         
         int left = 0, right = matrix.length - 1;
@@ -11,21 +10,17 @@ class Solution {
             else if (matrix[mid][0] > target) right = mid - 1;
             else if (matrix[mid][0] < target) left = mid;
         }
-        
         if (matrix[right][0] == target || matrix[left][0] == target) return true;
         
         int row = (target > matrix[right][0]) ? right : left;
-        
-        left = 0;
         right = matrix[0].length - 1;
+        left = 0;
         while (right - left > 1) {
             int mid = left + ((right - left) >> 1);
             if (matrix[row][mid] == target) return true;
             else if (matrix[row][mid] > target) right = mid - 1;
             else if (matrix[row][mid] < target) left = mid + 1;
         }
-        if (matrix[row][right] == target || matrix[row][left] == target)
-            return true;
-        return false;
+        return (matrix[row][right] == target || matrix[row][left] == target);
     }
 }
