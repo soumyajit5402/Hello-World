cbedcade30b41af1d10f5688363fa0d7d1a027d9
Soumyajit Das <soumyajitdas5402@gmail.com>
2023-02-02 02:05:56 +0530

    Time: 1 ms (99.66%), Space: 40.5 MB (97.31%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,16 +1,19 @@
 class Solution {
+    int[] dp;
+    List<String> wordDict;
     public boolean wordBreak(String s, List<String> wordDict) {
-        int[] dp = new int[s.length()];
-        checkWordBreak(s, 0, wordDict, dp);
+        this.wordDict = wordDict;
+        dp = new int[s.length()];
+        checkWordBreak(s, 0);
         return dp[0] == 1;
     }
     
-    private boolean checkWordBreak(String s, int index, List<String> wordDict, int[] dp) {
+    private boolean checkWordBreak(String s, int index) {
         if (index == s.length()) return true;
         if (dp[index] != 0) return dp[index] == 1;
         String newWord = s.substring(index);
         for (String word : wordDict) {
-            if (newWord.startsWith(word) && checkWordBreak(s, index + word.length(), wordDict, dp)) {
+            if (newWord.startsWith(word) && checkWordBreak(s, index + word.length())) {
                 dp[index] = 1;
                 return true;
             }
