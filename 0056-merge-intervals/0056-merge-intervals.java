6499885226bf87a9de22f8485da81e3510eddd34
Soumyajit Das <soumyajitdas5402@gmail.com>
2023-02-01 12:07:19 +0530

    Time: 8 ms (80.70%), Space: 46.8 MB (91.09%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,31 +1,19 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, (int[] a, int[] b) -> Integer.compare(a[0], b[0]));
-        Stack<int[]> container = new Stack<>();
-        container.push(intervals[0]);
+        Arrays.sort(intervals, (int[] a, int[] b) -> a[0] - b[0]);
+        List<int[]> ans = new ArrayList<>();
+        int[] curr = intervals[0];
         
         for (int i = 1; i < intervals.length; i++) {
-            if (isOverlappingInterval(container.peek(), intervals[i])) {
-                int[] mergedInterval = mergeIntervals(container.pop(), intervals[i]);
-                container.push(mergedInterval);
+            if (curr[1] >= intervals[i][0]) {
+                curr = new int[] { Math.min(curr[0], intervals[i][0]), Math.max(curr[1], intervals[i][1]) };
             } else {
-                container.push(intervals[i]);
-            }
+                ans.add(curr);
+                curr = intervals[i];
+            }   
         }
+        ans.add(curr);
         
-        int index = container.size() - 1;
-        int[][] result = new int[container.size()][2];
-        while (!container.isEmpty()) {
-            result[index--] = container.pop();
-        }
-        return result;
-    }
-    
-    private boolean isOverlappingInterval(int[] arr1, int[] arr2) {
-        return arr1[1] >= arr2[0];
-    }
-    
-    private int[] mergeIntervals(int[] arr1, int[] arr2) {
-        return new int[] { Math.min(arr1[0], arr2[0]), Math.max(arr1[1], arr2[1]) };
+        return ans.toArray(new int[ans.size()][2]);
     }
 }
