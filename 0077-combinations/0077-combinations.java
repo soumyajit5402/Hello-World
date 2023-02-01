5cee48fe06ec5b60a2817c436358607508c66c4d
Soumyajit Das <soumyajitdas5402@gmail.com>
2023-02-01 13:45:49 +0530

    Time: 19 ms (74.77%), Space: 45.1 MB (36.58%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,18 +1,17 @@
 class Solution {
     List<List<Integer>> container = new LinkedList<>();
     public List<List<Integer>> combine(int n, int k) {
-        findCombinations(1, k, n, new ArrayList<>());
+        findCombinations(1, k, n, new LinkedList<>());
         return container;
     }
     
-    private void findCombinations(int val, int len, int max, List<Integer> currList) {
-        if (currList.size() == len) container.add(currList);
+    private void findCombinations(int val, int len, int max, LinkedList<Integer> currList) {
+        if (currList.size() == len) container.add(new LinkedList<>(currList));
         if (currList.size() >= len || val > max) return;
         
-        List<Integer> included = new ArrayList<>(currList);
-        included.add(val);
-        findCombinations(val + 1, len, max, included);
-        List<Integer> excluded = new ArrayList<>(currList);
-        findCombinations(val + 1, len, max, excluded);
+        currList.add(val);
+        findCombinations(val + 1, len, max, currList);
+        currList.removeLast();
+        findCombinations(val + 1, len, max, currList);
     }
 }
