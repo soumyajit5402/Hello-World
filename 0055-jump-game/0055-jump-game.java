b5785d2ddd9cf443e13682101b0b40a74c073f1b
Soumyajit Das <soumyajitdas5402@gmail.com>
2023-02-01 11:51:53 +0530

    Time: 1 ms (100.00%), Space: 43.4 MB (19.67%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,16 +1,20 @@
 class Solution {
     public boolean canJump(int[] nums) {
+        int closestPossibleIndex = nums.length - 1;
         boolean[] dp = new boolean[nums.length];
         dp[nums.length - 1] = true;
         
         for (int i = nums.length - 2; i >= 0; i--) {
-            for (int j = i + 1; j <= Math.min(i + nums[i], nums.length - 1); j++) {
-                if (dp[j]) {
-                    dp[i] = true;
-                    break;
-                }
+            if (closestPossibleIndex <= i + nums[i]) {
+                closestPossibleIndex = i;
             }
+            // for (int j = i + 1; j <= Math.min(i + nums[i], nums.length - 1); j++) {
+            //     if (dp[j]) {
+            //         dp[i] = true;
+            //         break;
+            //     }
+            // }
         }
-        return dp[0];
+        return closestPossibleIndex == 0;
     }
 }
