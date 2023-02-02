e278423427d5041feb50685c3604c3f71ed76458
Soumyajit Das <soumyajitdas5402@gmail.com>
2023-02-02 16:42:01 +0530

    Time: 2 ms (57.82%), Space: 43.3 MB (95.72%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int left = 0, right = 0;
+        while (right < nums.length) {
+            if (left < right && nums[left] != 0) {
+                left++;
+            } else if (left != right && nums[right] != 0) {
+                nums[left++] = nums[right];
+                nums[right++] = 0;
+            } else {
+                right++;
+            }
+        }
+    }
+}
