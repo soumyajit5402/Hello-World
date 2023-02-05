84249f134ae602f7d23fde3d3330a538d621e4fc
Soumyajit Das <soumyajitdas5402@gmail.com>
2023-02-05 18:38:15 +0530

    Time: 14 ms (18.50%), Space: 43.1 MB (73.28%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.java
+++ 0463-island-perimeter/0463-island-perimeter.java
@@ -1,10 +1,6 @@
 class Solution {
     int[] x_diff = new int[] { 1, -1, 0, 0 };
     int[] y_diff = new int[] { 0, 0, 1, -1 };
-    
-    int[] x_diff_all = new int[] { -1, -1, -1, 0, 0, 1, 1, 1 };
-    int[] y_diff_all = new int[] { -1, 0, 1, -1, 1, -1, 0, 1 };
-    
     public int islandPerimeter(int[][] grid) {
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
@@ -16,19 +12,12 @@ class Solution {
     
     private int computePerimeter(int[][] grid, int x, int y) {
         int count = 0;
+        grid[x][y] = -1;
         for (int i = 0; i < 4; i++) {
             int newX = x + x_diff[i];
             int newY = y + y_diff[i];
             if (isPerimeter(grid, newX, newY)) count++;
-        }
-        
-        grid[x][y] = -1;
-        for (int i = 0; i < 8; i++) {
-            int newX = x + x_diff_all[i];
-            int newY = y + y_diff_all[i];
-            if (isEligibleCell(grid, newX, newY)) {
-                count += computePerimeter(grid, newX, newY);
-            }
+            if (isEligibleCell(grid, newX, newY)) count += computePerimeter(grid, newX, newY);
         }
         return count;
     }
