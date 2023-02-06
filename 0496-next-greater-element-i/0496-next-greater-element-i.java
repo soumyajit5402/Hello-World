72db6dedd947dfc6a771a7f3ee1c18e73bef051b
Soumyajit Das <soumyajitdas5402@gmail.com>
2023-02-06 11:57:48 +0530

    Time: 3 ms (87.50%), Space: 42.8 MB (32.51%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> container = new HashMap<>();
+        for (int i = 0; i < nums2.length; i++) {
+            container.put(nums2[i], i);
+        }
+        
+        int[] nextGreaterIndex = new int[nums2.length];
+        Stack<Integer> nextGreater = new Stack<>();
+        for (int i = 0; i < nums2.length; i++) {
+            while (!nextGreater.isEmpty() && nums2[i] > nums2[nextGreater.peek()]) {
+                nextGreaterIndex[nextGreater.pop()] = nums2[i];
+            }
+            nextGreater.push(i);
+        }
+        while (!nextGreater.isEmpty()) {
+            nextGreaterIndex[nextGreater.pop()] = -1;
+        }
+        
+        int[] ans = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            int index2 = container.get(nums1[i]);
+            ans[i] = nextGreaterIndex[index2];
+        }
+        return ans;
+    }
+}
