23f4025306521e48bec8e68e169569cc2365024e
Soumyajit Das <soumyajitdas5402@gmail.com>
2023-02-06 12:19:02 +0530

    Time: 0 ms (100.00%), Space: 43.2 MB (52.41%) - LeetHub

--- 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.java
+++ 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.java
@@ -1,13 +1,9 @@
 class Solution {
     List<Integer> container = new ArrayList<>();
     public List<Integer> preorder(Node root) {
-        traverse(root);
+        if (root == null) return container;
+        container.add(root.val);
+        for (Node child : root.children) preorder(child);
         return container;
-    }
-    
-    private void traverse(Node node) {
-        if (node == null) return;
-        container.add(node.val);
-        for (Node child : node.children) traverse(child);
     }
 }
