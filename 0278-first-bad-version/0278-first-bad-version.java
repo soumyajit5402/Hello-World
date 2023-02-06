8e3c2927095ec9f7b58891d84a87adb7ea9ee7b2
Soumyajit Das <soumyajitdas5402@gmail.com>
2023-02-06 12:30:08 +0530

    Time: 17 ms (66.26%), Space: 38.7 MB (94.77%) - LeetHub

---
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -0,0 +1,12 @@
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int left = 1, right = n;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid) == true) right = mid;
+            else left = mid + 1;
+        }
+        if (isBadVersion(left)) return left;
+        return right;
+    }
+}
