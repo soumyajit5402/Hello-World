c476cf58937abbb4cad32ac8a08e7cf8e7314478
Soumyajit Das <soumyajitdas5402@gmail.com>
2023-02-08 03:41:03 +0530

    Time: 105 ms (69.41%), Space: 50.6 MB (92.94%) - LeetHub

---
+++ 2407-longest-increasing-subsequence-ii/2407-longest-increasing-subsequence-ii.java
@@ -0,0 +1,89 @@
+class Solution {
+    public int lengthOfLIS(int[] nums, int k) {
+        int segTreeLen = 1 + Arrays.stream(nums).max().getAsInt();
+        MaxSegmentTree maxLISSegmentTree = new MaxSegmentTree(segTreeLen);
+        
+        int maxLISLen = 1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) maxLISSegmentTree.updateTree(1, 1);
+            else {
+                int left = Math.max(1, nums[i] - k);
+                int right = nums[i] - 1;
+                int maxInRange = maxLISSegmentTree.getMax(left, right);
+                maxLISSegmentTree.updateTree(nums[i], maxInRange + 1);
+                maxLISLen = Math.max(maxLISLen, maxInRange + 1);    
+            }
+        }
+        return maxLISLen;
+    }
+    
+    class MaxSegmentTree {
+    
+        int len;
+        int[] arr;
+
+        public MaxSegmentTree(int size) {
+            len = size;
+            arr = new int[4 * size];
+        }
+
+        public int constructTree(int[] input, int arrStartIndex, int arrEndIndex, int segTreeIndex) {
+            if (arrStartIndex == arrEndIndex) {
+                arr[segTreeIndex] = input[arrStartIndex];
+                return arr[segTreeIndex];
+            }
+
+            int arrMidIndex = arrStartIndex + ((arrEndIndex - arrStartIndex) >> 1);
+
+            arr[segTreeIndex] = Math.max(constructTree(input, arrStartIndex, arrMidIndex, 2 * segTreeIndex),
+                                      constructTree(input, arrMidIndex + 1, arrEndIndex, 2 * segTreeIndex + 1));
+
+            return arr[segTreeIndex];
+        }
+
+        private void updateTreeUtil(int startIndex, int endIndex, int updateIndex, int newValue, int segTreeIndex) {
+            if (updateIndex < startIndex || updateIndex > endIndex) {
+                return;
+            }
+
+            int midIndex = startIndex + ((endIndex - startIndex) >> 1);
+            arr[segTreeIndex] = Math.max(arr[segTreeIndex], newValue);
+
+            if (startIndex == endIndex) return;
+
+            updateTreeUtil(startIndex, midIndex, updateIndex, newValue, 2 * segTreeIndex);
+            updateTreeUtil(midIndex + 1, endIndex, updateIndex, newValue, 2 * segTreeIndex + 1);
+        }
+
+        public void updateTree(int index, int newValue) {
+            updateTreeUtil(0, len - 1, index, newValue, 1);
+        }
+
+        private int getMaxUtil(int startIndex, int endIndex, int queryStartIndex, int queryEndIndex, int segTreeIndex) {
+            if (queryStartIndex <= startIndex && queryEndIndex >= endIndex) {
+                return arr[segTreeIndex];
+            }
+
+            if (queryEndIndex < startIndex || queryStartIndex > endIndex) {
+                return 0;
+            }
+
+            int midIndex = startIndex + ((endIndex - startIndex) >> 1);
+
+            return Math.max(getMaxUtil(startIndex, midIndex, queryStartIndex, queryEndIndex, 2 * segTreeIndex),
+                            getMaxUtil(midIndex + 1, endIndex, queryStartIndex, queryEndIndex, 2 * segTreeIndex + 1));
+        }                               
+
+        public int getMax(int queryStartIndex, int queryEndIndex) {
+            return getMaxUtil(0, len - 1, queryStartIndex, queryEndIndex, 1);
+        }
+
+        public void print() {
+            for (int num : arr) {
+                System.out.print(num + ", ");
+            }
+            System.out.println();
+        }
+    }
+}
+
