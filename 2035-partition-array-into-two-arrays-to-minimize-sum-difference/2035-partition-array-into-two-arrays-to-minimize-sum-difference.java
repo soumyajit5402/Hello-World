59beca539d62ac022d7ab9807136eebd632ae6a7
Soumyajit Das <soumyajitdas5402@gmail.com>
2023-02-08 00:46:29 +0530

    Time: 1203 ms (23.03%), Space: 51.9 MB (32.89%) - LeetHub

--- 2035-partition-array-into-two-arrays-to-minimize-sum-difference/2035-partition-array-into-two-arrays-to-minimize-sum-difference.java
+++ 2035-partition-array-into-two-arrays-to-minimize-sum-difference/2035-partition-array-into-two-arrays-to-minimize-sum-difference.java
@@ -1,7 +1,7 @@
 class Solution {
     public int minimumDifference(int[] nums) {
-        TreeSet<Integer>[] containerLeft = new TreeSet[1 + (nums.length >> 1)];
-        TreeSet<Integer>[] containerRight = new TreeSet[1 + (nums.length >> 1)];
+        Set<Integer>[] containerLeft = new Set[1 + (nums.length >> 1)];
+        Set<Integer>[] containerRight = new Set[1 + (nums.length >> 1)];
         
         findAllSums(nums, 0, (nums.length >> 1) - 1, 0, 0, containerLeft);
         findAllSums(nums, (nums.length >> 1), nums.length - 1, 0, 0, containerRight);
@@ -14,7 +14,7 @@ class Solution {
         int total = Arrays.stream(nums).sum();
         for (int leftLen = 0; leftLen <= nums.length/2; leftLen++) {
             int rightLen = (nums.length/2) - leftLen;
-            TreeSet<Integer> rightVals = containerRight[rightLen];
+            TreeSet<Integer> rightVals = new TreeSet<>(containerRight[rightLen]);
             for (int val : containerLeft[leftLen]) {
                 int delta = ((total - (val << 1)) >> 1);
                 Integer floorVal = rightVals.floor(delta);
@@ -26,8 +26,8 @@ class Solution {
         return minDiff;
     }
     
-    private void findAllSums(int[] arr, int left, int right, int count, int sum, TreeSet<Integer>[] container) {
-        if (container[count] == null) container[count] = new TreeSet<>();
+    private void findAllSums(int[] arr, int left, int right, int count, int sum, Set<Integer>[] container) {
+        if (container[count] == null) container[count] = new HashSet<>();
         container[count].add(sum);
         if (left > right) return;
         findAllSums(arr, left + 1, right, count + 1, sum + arr[left], container);
