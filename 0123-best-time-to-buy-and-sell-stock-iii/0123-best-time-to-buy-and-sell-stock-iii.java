8a6c320c4301ce92a67f5a0727eae0abe1a4f505
Soumyajit Das <soumyajitdas5402@gmail.com>
2023-02-13 19:52:33 +0530

    Time: 4 ms (70.70%), Space: 51.1 MB (94.83%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,19 +1,18 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int[] maxProfit = new int[prices.length];
         int maxRight = prices[prices.length - 1];
+        int[] maxProfit1Trxn = new int[prices.length];
         for (int i = prices.length - 2; i >= 0; i--) {
-            maxProfit[i] = Math.max(0, maxRight - prices[i]);
+            maxProfit1Trxn[i] = Math.max(0, maxRight - prices[i]);
             maxRight = Math.max(maxRight, prices[i]);
         }
         
         int[] maxProfitRight = new int[prices.length];
         for (int i = prices.length - 2; i >= 0; i--) {
-            maxProfitRight[i] = Math.max(maxProfit[i], maxProfitRight[i+1]);
+            maxProfitRight[i] = Math.max(maxProfit1Trxn[i], maxProfitRight[i+1]);
         }
         
-        int minLeft = prices[0];
-        int totalMaxProfit = 0;
+        int minLeft = prices[0], totalMaxProfit = 0;
         for (int i = 0; i < prices.length - 1; i++) {
             int currProfit = Math.max(0, prices[i] - minLeft) + maxProfitRight[i];
             totalMaxProfit = Math.max(totalMaxProfit, currProfit);
