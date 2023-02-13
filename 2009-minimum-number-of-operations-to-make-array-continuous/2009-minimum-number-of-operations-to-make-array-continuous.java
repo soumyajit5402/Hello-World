ba3732fca56b4334765ff012119bc098357faec3
Soumyajit Das <soumyajitdas5402@gmail.com>
2023-02-13 14:38:16 +0530

    Time: 139 ms (25.00%), Space: 58.7 MB (87.50%) - LeetHub

--- 2009-minimum-number-of-operations-to-make-array-continuous/2009-minimum-number-of-operations-to-make-array-continuous.java
+++ 2009-minimum-number-of-operations-to-make-array-continuous/2009-minimum-number-of-operations-to-make-array-continuous.java
@@ -1,22 +1,24 @@
 class Solution {
     public int minOperations(int[] nums) {
+        // sort input
         Arrays.sort(nums);
+        // extract all unique values into a list in sorted order
         List<Integer> container = new ArrayList<>();
         container.add(nums[0]);
         for (int i = 1; i < nums.length; i++) if (nums[i] != nums[i-1]) container.add(nums[i]);
 
-        int minChanges = Integer.MAX_VALUE;
-        for (int i = 0; i < container.size(); i++) {
+        // assuming the answer starts at each element in the list, compute minimum changes required
+        int minChanges = nums.length - 1;
+        for (int i = 0; i < container.size() - 1; i++) {
             int target = container.get(i) + nums.length;
-            int index = binarySearch(container, target);
+            int index = binarySearch(container, i + 1, container.size() - 1, target);
             int changes = nums.length - (index - i);
             minChanges = Math.min(minChanges, changes);   
         }
         return minChanges;
     }
     
-    public int binarySearch(List<Integer> list, int val) {
-        int left = 0, right = list.size() - 1;
+    public int binarySearch(List<Integer> list, int left, int right, int val) {
         while (left < right) {
             int mid = left + (right - left) / 2;
             if (list.get(mid) == val) return mid;
