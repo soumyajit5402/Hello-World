bfd5f2da1faf489da33d8b1eeefdd3261a45970b
Soumyajit Das <soumyajitdas5402@gmail.com>
2023-03-09 11:33:44 +0530

    Time: 0 ms (100.00%), Space: 39.3 MB (67.03%) - LeetHub

---
+++ 2582-pass-the-pillow/2582-pass-the-pillow.java
@@ -0,0 +1,6 @@
+class Solution {
+    public int passThePillow(int n, int time) {
+        if ((time / (n - 1)) % 2 == 0) return ((time % (n - 1)) + 1);
+        else return n - ((time % (n - 1))); 
+    }
+}
